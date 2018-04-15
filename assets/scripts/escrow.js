502b2ad8cea272415d0e1cf49f90ca6659450838
Tomisin <tomisinjenrola@gmail.com>
2018-04-15 08:14:34 -0230

    refactor

--- assets/scripts/escrow.js
+++ assets/scripts/escrow.js
@@ -1,95 +1,27 @@
-const $ = require('jquery');
-const contract = require('truffle-contract');
-const escrow_artifact = require('../../build/contracts/Escrow.json');
-const Escrow = contract(escrow_artifact);
+$(document).ready(function() {
+  // const $().val();
 
+  let clientAccount;
+  let amount;
+  let freelancerAccount;
 
+  $("#create").click(function() {
+    clientAccount = $("#clientAddress").val();
+    amount = $("#amount").val();
+    console.log(clientAccount + " : " + amount);
+    $.post('/create', {account: clientAccount, amount: amount}, function(response) {
+      console.log("sent")
+    })
+  });
 
-module.exports = {
-    init: function() {
-        let self = this;
+  $("#complete").click(function() {
+    freelancerAccount = $("#freelancerAddress");
+    console.log(freelancerAccount);
 
-        Escrow.setProvider(self.web3.currentProvider);
-        if (typeof Escrow.currentProvider.sendAsync !== "function") {
-            Escrow.currentProvider.sendAsync = function() {
-              return Escrow.currentProvider.send.apply(
-                Escrow.currentProvider, arguments
-              );
-            };
-        }
+  });
 
-        // Escrow.defaults({from: self.web3.eth.coinbase});
+  $("#terminate").click(function() {
 
-        self.web3.eth.getAccounts().then(accounts => {
-            console.log(accounts)
-            self.web3.eth.defaultAccount = accounts[0]
+  });
 
-            // Escrow.defaults(accounts[0]);
-        }); // test
-    },
-    setClient: function(clientAddress) {
-        let self = this;
-        console.log("setClient:\t" + clientAddress + "default:\t" + self.web3.eth.defaultAccount);
-        
-        self.client = clientAddress;
-        Escrow.defaults(self.client);
-
-        Escrow.setProvider(self.web3.currentProvider);
-        
-        let escrowInstance;
-        Escrow.deployed().then((instace) => {
-            escrowInstance = instace;
-            escrowInstance.setClient(clientAddress);
-        }).then(() => {
-            console.log(escrowInstance.client);
-        }).catch((err) => {
-            console.log("error in setClient");
-            console.log(err);
-        });
-    },
-    setFreelancer: function(freelancerAddress) {
-        let escrowInstance;
-        Escrow.deployed().then((instace) => {
-            escrowInstance = instace;
-            escrowInstance.setFreelancer(freelancerAddress);
-        }).then(() => {
-            console.log(escrowInstance.freelancer);
-        }).catch((err) => {
-            console.log(err);
-        });
-    },
-    start: function(amount) {
-        let self = this;
-        console.log("start:\t" + amount);
-        let escrowInstance;
-        Escrow.defaults(self.client);
-        Escrow.deployed().then((instace) => {
-            escrowInstance = instace;
-            escrowInstance.start();
-        }).catch((err) => {
-            console.log("error in start");
-            console.log(err);
-        });
-    },
-    complete: function() {
-        let escrowInstance;
-        Escrow.deployed().then((instace) => {
-            escrowInstance = instace;
-            escrowInstance.complete();
-        }).then(() => {
-            console.log(escrowInstance.state);
-            console.log(escrowInstance.payment);
-        }).catch((err) => {
-            console.log(err);
-        });
-    },
-    terminate: function() {
-        let escrowInstance;
-        Escrow.deployed().then((instace) => {
-            escrowInstance = instace;
-            escrowInstance.terminate();
-        }).catch((err) => {
-            console.log(err);
-        });
-    }
-};
+});
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000 || process.env.PORT;
 const bodyParser = require('body-parser');
 
 const Web3 = require('web3');
-const escrow = require('./assets/scripts/escrow.js');
+const truffle_connect = require('./connection/app.js');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,17 +18,17 @@ app.get('/', (req, res) => {
 
 app.post('/create', (req, res) => {
   console.log("/create route:\t" + req.body.clientAddress);
-  escrow.setClient(req.body.clientAddress);
-  escrow.start(req.body.amount);
+  truffle_connect.setClient(req.body.clientAddress);
+  truffle_connect.start(req.body.amount);
 });
 
 app.post('/complete', (req, res) => {
-  escrow.setFreelancer(req.body.FreelancerAddress);
-  escrow.complete();
+  truffle_connect.setFreelancer(req.body.FreelancerAddress);
+  truffle_connect.complete();
 });
 
 app.post('/terminate', (req, res) => {
-  escrow.terminate();
+  truffle_connect.terminate();
 });
 
 
@@ -37,13 +37,13 @@ app.listen(port, () => {
   if (typeof web3 !== 'undefined') {
     console.warn("Using web3 detected from external source. If using MetaMask, see the following link - http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
-    escrow.web3 = new Web3(web3.currentProvider);
+    truffle_connect.web3 = new Web3(web3.currentProvider);
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    escrow.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    escrow.init();
-    // escrow.web3.eth.getAccounts().then(accounts => console.log(accounts)); // test
+    truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    truffle_connect.init();
+    // truffle_connect.web3.eth.getAccounts().then(accounts => console.log(accounts)); // test
   }
   
   console.log("listening at http://localhost:" + port);
